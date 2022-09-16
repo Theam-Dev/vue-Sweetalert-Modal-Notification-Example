2c5d56c7e05ed486707e1ce5b6848ff505195609
Pin Phearak <phearakroun@gmail.com>
2022-09-16 18:16:47 +0700

    Vue Sweetalert

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,49 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div class="container p-2">
+  <h4>A basic Message</h4>
+  <button @click="basicAlert" class="btn btn-primary">Basic Message</button>
+  <h4>A basic Error Message</h4>
+  <button @click="showAlertError" class="btn btn-success">Basic Error</button>
+  <h4>Yes No Question Message</h4>
+  <button @click="yesNoQuestion" class="btn btn-info">Yes No Quest</button>
+  </div>
 </template>
-
 <script>
-import HelloWorld from './components/HelloWorld.vue'
 
 export default {
   name: 'App',
-  components: {
-    HelloWorld
+  methods:{
+    basicAlert() {
+      this.$swal('Hello Vue world!!!');
+    },
+    showAlertError(){
+      this.$swal({
+        icon: 'error',
+        title: 'Oops...',
+        text: 'Something went wrong!',
+      })
+    },
+    yesNoQuestion(){
+      this.$swal({
+        title: "Are you sure?",
+        text: "You can't revert your action",
+        type: "warning",
+        icon: 'warning',
+        showCancelButton: true,
+        confirmButtonText: "Yes Delete",
+        cancelButtonText: "Cancel",
+        showCloseButton: true,
+        showLoaderOnConfirm: true,
+      }).then((result) => {
+        if (result.value) {
+          this.$swal(
+            'Deleted!',
+            'Your file has been deleted.',
+            'success'
+          )
+        }
+      });
+    }
   }
 }
 </script>
-
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
--- src/main.js
+++ src/main.js
@@ -1,4 +1,8 @@
 import { createApp } from 'vue'
 import App from './App.vue'
+import VueSweetalert2 from 'vue-sweetalert2';
+import "bootstrap/dist/css/bootstrap.min.css";
+import "bootstrap/dist/js/bootstrap.min.js";
+import 'sweetalert2/dist/sweetalert2.min.css';
 
-createApp(App).mount('#app')
+createApp(App).use(VueSweetalert2).mount('#app')
